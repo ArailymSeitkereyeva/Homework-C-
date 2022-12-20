be471a81d47cad074873d5566eec6a3245470e4e
Arai <seit.arailym@gmail.com>
2022-12-21 00:06:37 +0300

    hw 1

--- homework001/Program.cs
+++ homework001/Program.cs
@@ -1,14 +1,20 @@
 ﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-int a = 2;
-int b = 3;
-int c = 7;
-
-int max = a;
-
-if ( a > max ) max = a;
-if ( b > max ) max = b;
-if ( c > max ) max = c;
-
-Console.Write("max = ");
-Console.WriteLine(max);
+int size = 3;
+int[] numbs = new int[size];
+int count = 0;
+while(count < size)
+{
+    Console.WriteLine($"Введите {1+count} число:");
+    numbs[count] = Convert.ToInt32(Console.ReadLine());
+    count++;
+}
+count = 1;
+int max = numbs[0];
+while(count < size)
+{
+if(numbs[count]>max)
+max = numbs[count];
+count++;
+}
+Console.WriteLine($"Максимальное число: "+max);
